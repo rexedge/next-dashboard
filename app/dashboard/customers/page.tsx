29ea9e193251860cb9a5c210b4996c6b1bcc1026
Rexedge <ap.oyeniran@gmail.com>
2023-10-28 11:35:54 +0100

    completed project

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,28 @@
-export default function CustomersPage() {
-	return <p>Customers Page</p>;
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+export const metadata: Metadata = {
+	title: 'Customers',
+};
+
+export default async function Page({
+	searchParams,
+}: {
+	searchParams?: {
+		query?: string;
+	};
+}) {
+	const query = searchParams?.query || '';
+
+	const customers = await fetchFilteredCustomers(query);
+
+	return (
+		<div className='w-full'>
+			<Suspense fallback={<InvoicesTableSkeleton />}>
+				<CustomersTable customers={customers} />
+			</Suspense>
+		</div>
+	);
 }
